d92826514349b94b522d462b3cca9161f6036db6
IceSword8492 <cleared.for.approach.runway23@gmail.com>
2019-06-13 13:14:16 +0900

    updated

--- shop.h
+++ shop.h
@@ -10,12 +10,12 @@ typedef struct Shop {
     int amount[3];
 } Shop;
 
-Shop new_Shop (int p[3], int a[3])
+Shop new_Shop (int p1, int p2, int p3, int a1, int a2, int a3)
 {
     Shop s = {
         &s,
-        p,
-        a
+        {p1, p2, p3},
+        {a1, a2, a3}
     };
     return s;
 }
