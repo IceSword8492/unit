1f8561fd13e40eeb958a49bb0436046614b0b289
IceSword8492 <cleared.for.approach.runway23@gmail.com>
2019-06-14 16:09:34 +0900

    updated

--- battle.cpp
+++ battle.cpp
@@ -21,6 +21,7 @@
 #define STUDENT_SKILLS {0, 0}
 
 int state, prevState;
+float tmpAttack;
 
 void setState (int s)
 {
@@ -61,6 +62,63 @@ Student new_Student (const char* name)
     return s;
 }
 
+typedef struct Enemy
+{
+    struct Enemy* self; // 自身
+    int hp;             // HP
+    int maxHp;          // 最大HP
+    float stdAtk;       // 基礎攻撃力
+    bool charge;        // 溜めの有無
+    int dmgCut;         // 半減の残りターン数
+    int intelligence;   // もらえる知識量
+    int type;           // 中:1, 通常:2, 裏ボス:3か雑魚:0か
+    int act;            // 行動ルーチン
+    int spact;          // 特殊行動
+} Enemy;
+
+Enemy new_Enemy (int hp, int maxHp, float stdAtk, int intelligence, int type, int act, int spact)
+{
+    Enemy e = {
+        &e,
+        hp,
+        maxHp,
+        stdAtk,
+        0,
+        0,
+        intelligence,
+        type,
+        act,
+        spact
+    };
+    return e;
+}
+
+Enemy enemies[9]; 
+
+#define SKILL_F1B 1
+#define SKILL_F2B 2
+#define SKILL_F3B 3
+#define SKILL_F4B 4
+#define ROUTINE_F4B 5
+#define SKILL_LAST 6
+#define ROUTINE_LAST 7
+//敵の初期化by吉岡誇太郎
+void initializeEnemies(){
+    Enemy e[9] = {
+        new_Enemy(500,500,500,100,0,0,0),//f1z
+        new_Enemy(1000,1000,1000,400,1,0,SKILL_F1B),//f1b
+        new_Enemy(2000,2000,500,200,0,0,0),//f2z
+        new_Enemy(4500,4500,1000,600,1,0,SKILL_F2B),//f2b
+        new_Enemy(4000,4000,800,300,0,0,0),//f3z
+        new_Enemy(10000,10000,1000,800,1,0,SKILL_F3B),//f3b
+        new_Enemy(10000,10000,1000,450,0,0,0),//f4z
+        new_Enemy(15000,15000,1000,1000,2,ROUTINE_F4B,SKILL_F4B),//f4b
+        new_Enemy(50000,50000,2000,0,3,ROUTINE_LAST,SKILL_LAST),//last
+    };
+    memcpy(enemies, e, sizeof(e));
+}
+
+
 typedef struct Cursor {
     int pos;
 } Cursor;
@@ -68,8 +126,20 @@ typedef struct Cursor {
 Cursor cursor;
 Student player;
 
+void addIntelligence(int intelligence){ 
+    player.intelligence+=intelligence;
+}
+
+
+bool win;
+bool lose;
+bool trueClear;
+
 #include "useItem.c"
 #include "useSkill.c"
+#include "damage.c"
+#include "isCritical.c"
+#include "battleVictory.c"
 
 void execute ()
 {
@@ -83,6 +153,9 @@ void execute ()
 
 int main (int argc, const char** argv)
 {
+    win = false;
+    lose = false;
+    trueClear = false;
     cursor.pos = 0;
     player = new_Student("TestStudent");
     player.intelligence = 300;
@@ -91,6 +164,7 @@ int main (int argc, const char** argv)
     player.item[2] = 1;
     state = D_BATTLE;
     srand(time(NULL));
+    initializeEnemies();
     rand();
     for (;;)
     {
