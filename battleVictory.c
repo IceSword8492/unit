381e9d4f6013c3c1bf60b93420675088bad6d141
00547daic <c011704994@edu.teu.ac.jp>
2019-06-27 03:10:12 +0900

    Victry add skillrecast

--- battleVictory.c
+++ battleVictory.c
@@ -5,6 +5,10 @@ void battleVictory ()
     Enemy enemy = *getEnemy();
     addIntelligence(enemy.intelligence);
     setState(D_DUNGEON);
+    player.recast[0] = 0;
+    player.recast[1] = 0;
+    player.recast[2] = 0;
+    player.recast[3] = 0;
     if(enemy.type == 1)
     {
         player.pos[0]++;
