ab323c7ccef33dfb65e579db02fbd1133c3c1ee1
00547daic <c011704994@edu.teu.ac.jp>
2019-07-24 17:17:14 +0900

    クリアまたはゲームオーバーした際にcalcRemainの計算方法を変更

--- calcRemain.cpp
+++ calcRemain.cpp
@@ -6,6 +6,15 @@
 
 int calcRemain () // returns remain time millisec
 {
+    int finish;
+    if (win || lose)
+    {
+        if (finish == 0)
+        {
+            finish = now;
+        }
+        return limitTime -(finish - startTime) - debugTime;
+    }
     return limitTime - (now - startTime) - debugTime;
 }
 
